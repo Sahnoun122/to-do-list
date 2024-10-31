c95da610ea72d995b852efcc9e5ce28388e88d55
Sahnoun122 <khadijashnoun70@gmail.com>
2024-10-31 17:28:44 +0100

    trouver la solution de selection chaque mission

--- views/todo.js
+++ views/todo.js
@@ -12,19 +12,22 @@ let btn = function(){
    
 }
 
-let title = document.getElementById('title');
+ let title = document.getElementById('title');
 let statu = document.getElementById('statu');
 let Priority = document.getElementById('Priority');
 let description = document.getElementById('description');
 let submit = document.getElementById('submit');
 let todo1 = document.getElementById('todo1');
+let doing1 = document.getElementById('doing1');
+let done1 = document.getElementById('doing1');
+
 let box= document.querySelector('.box');
 
 
 // create task 
    
   
-    let data
+    let data;
     if(localStorage.stockage != null){
         data= JSON.parse(localStorage.stockage);
     }else{
@@ -33,10 +36,10 @@ let box= document.querySelector('.box');
         
 
  
-  submit.onclick = function(){
-   let  newtask = {
+    submit.onclick = function(){
+    let  newtask = {
     title:title.value,
-    status: statu.value,
+    statu: statu.value,
     Priority:Priority.value,
     description: description.value,
 
@@ -47,77 +50,70 @@ let box= document.querySelector('.box');
 }
 
 
-// read data 
+ // read data 
 
-let continer= document.getElementById("todo1");
-console.log(continer);
+ submit.onclick=function (){
+
+    const container = getcontainer(statu.value);
+
+        if(container){
+            container.innerHTML +=`
+        
+            <div  id="pro" class="border-2 ..." ${createHtml(Priority)} style="height: 130px; width: 330px; margin-top:60px ; margin-left: 150px;">
+           
+                <h2 style="margin-left: 30px; font-family: 'Courier New', Courier, monospace;   font-size: 600;">${title.value}</h2>
+                                        <p style="margin-left: 20px;">${statu.value}</p>
+                                       <p style="margin-left: 20px;">${Priority.value}</p>
+           
+                                        <p style="margin-left: 20px;">${description.value}</p>
+           
+           
+
+                   <div class="flex" style="margin-top: 60px; margin-left: 140px;">
+           
+                       <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</button>
+                       <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+           
+           
+                   </div>
+                   </div>
+           `
+
+        }
+
+       
+    }
 
 
-submit.onclick = function(){
-continer.innerHTML +=`
- <div class="border-2 border-red-600  border-l-8 border-red-500..." style="height: 130px; width: 330px; margin-top:20px ; margin-left: 150px;">
+    function getcontainer(statu) {
+        switch (statu) {
+            case 'todo':
+              return  document.getElementById('todo1');
+                
+            case 'doing':
+                return document.getElementById('doing1');
+            case 'done':
+                return document.getElementById('done1');
+            default:
+                return null;
+        }
+    }
+   
+    
+           //delete
+    function deletedata(){
+             
+           }
 
-     <h2 style="margin-left: 30px; font-family: 'Courier New', Courier, monospace;   font-size: 600;">${title.value}</h2>
-                             <p style="margin-left: 20px;">${statu.value}</p>
-                            <p style="margin-left: 20px;">${Priority.value}</p>
+//update
 
-                             <p style="margin-left: 20px;">${description.value}</p>
+function updatedata(newtask){
+    title.value= data.title;
+    statu.value=data.statu;
+    Priority.value = data.Priority;
+    description.value = data.description;
 
-
-        <div class="flex" style="margin-top: 35px; margin-left: 140px;">
-
-            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</button>
-            <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
-
-
-        </div>
-        </div>
-`
 }
 
-let continer2= document.getElementById("doing1");
-console.log(continer2);
 
-submit.onclick = function(){
-    continer2.innerHTML +=`
-
-     <div class="border-2 border-blue-600  border-l-8 border-blue-500..." style="height: 130px; width: 330px; margin-top:20px ; margin-left: 150px;">
-
-      <h2 style="margin-left: 30px; font-family: 'Courier New', Courier, monospace;   font-size: 600;">${title.value}</h2>
-                             <p style="margin-left: 20px;">${statu.value}</p>
-                            <p style="margin-left: 20px;">${Priority.value}</p>
-
-                             <p style="margin-left: 20px;">${description.value}</p>
-
-        <div class="flex" style="margin-top: 80px; margin-left: 140px;">
-            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</button>
-
-            <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
-
-
-        </div>
-    </div>
-    `
-    }
-
-    let continer3= document.getElementById("done1");
-    console.log(continer3);
-
-    submit.onclick = function(){
-    continer3.innerHTML +=`
-
-      <div class="border-2 border-green-600  border-l-8 border-green-500..." style="height: 130px; width: 330px; margin-top:70px ; margin-left: 150px;">
-
-          <h2 style="margin-left: 30px; font-family: 'Courier New', Courier, monospace;   font-size: 600;">${title.value}</h2>
-                             <p style="margin-left: 20px;">${statu.value}</p>
-                            <p style="margin-left: 20px;">${Priority.value}</p>
-
-                             <p style="margin-left: 20px;">${description.value}</p>
-        <div class="flex" style="margin-top: 80px; margin-left: 140px;">
-            <button type="button" id="update" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</button>
-
-            <button type="button" id="delete" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
-        </div>
-    </div>
-    `
-    }
+           
